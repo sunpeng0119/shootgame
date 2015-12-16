6b9d93c2f1e7bbcc77240f1a275a979413df30e1
sunpeng <625657100@qq.com>
2015-12-16 09:37:54 +0800

    暂时去掉handler

--- shootgame/app/src/main/java/com/example/sunpeng/shootgame/MainActivity.java
+++ shootgame/app/src/main/java/com/example/sunpeng/shootgame/MainActivity.java
@@ -2,18 +2,19 @@ package com.example.sunpeng.shootgame;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends Activity {
 
     private UIHandler mUIHandler;
-    private WorkHandler mWorkHander;
+    //private WorkHandler mWorkHander;workhandler定义
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,37 +24,32 @@ public class MainActivity extends Activity {
         final EnemyPlane  enmy[];
         enemyPlane.currentX=50;
         enemyPlane.currentY=50;
-        //        enemyPlane.setBackgroundColor(Color.RED);
-        //enemyPlane.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT));
         mUIHandler=new UIHandler(Looper.getMainLooper());
-        HandlerThread thread=new HandlerThread("my work");
-        thread.start();
-        mWorkHander=new WorkHandler(thread.getLooper());
-//        Timer t1=new Timer();
-//        int time1=10000;
-//        t1.schedule(new TimerTask() {
-//            @Override
-//            public void run() {
-                mUIHandler.obtainMessage(1001,enemyPlane).sendToTarget();
-//                MainActivity.this.runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        enemyPlane.currentY+=100;
-//                        enemyPlane.invalidate();
-//                    }
-//                });
+        //HandlerThread thread=new HandlerThread("my work");
+        //thread.start();
+        //mWorkHander=new WorkHandler(thread.getLooper());
+        Timer t1=new Timer();
+        int time1=10;
+        t1.schedule(new TimerTask() {
+            @Override
+            public void run() {
+               // mUIHandler.obtainMessage(1001,enemyPlane).sendToTarget();
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        enemyPlane.currentY+=1;
+                        enemyPlane.invalidate();
+                    }
+                });
 
-//            }
-//        },10,time1);
+            }
+        },time1,time1);
 
-        mWorkHander.sendEmptyMessageDelayed(MSG_REFRESH_START,10000);
-        mWorkHander.sendEmptyMessage(MSG_REFRESH_START);
+       // mWorkHander.sendEmptyMessageDelayed(MSG_REFRESH_START,10000);
+        //mWorkHander.sendEmptyMessage(MSG_REFRESH_START);
         root.addView(enemyPlane);
 
         final Ariplane ariplane=new Ariplane(this);
-       // setContentView(ariplane);
-        //ariplane.setBackgroundResource(R.drawable.background);
-//        ariplane.setBackgroundColor(Color.GREEN);
         ariplane.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -90,23 +86,23 @@ public class MainActivity extends Activity {
             }
         }
     }
-    public static final int MSG_WORK_ONE1=1;
-    private static final int MSG_REFRESH_START=2;
-    private static final int MSG_INtERVAL=3;
-    private class WorkHandler extends Handler{
-        public WorkHandler(Looper looper){
-            super(looper);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what){
-                case MSG_WORK_ONE1:{
-                    //do something
-                    mWorkHander.sendEmptyMessageDelayed(MSG_WORK_ONE1,1000);
-                }
-            }
-        }
-    }
+//    public static final int MSG_WORK_ONE1=1;
+//    private static final int MSG_REFRESH_START=2;
+//    private static final int MSG_INtERVAL=3;
+//    private class WorkHandler extends Handler{
+//        public WorkHandler(Looper looper){
+//            super(looper);
+//        }
+//
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//            switch (msg.what){
+//                case MSG_WORK_ONE1:{
+//                    //do something
+//                    mWorkHander.sendEmptyMessageDelayed(MSG_WORK_ONE1,1000);
+//                }
+//            }
+//        }
+//    }
 }
